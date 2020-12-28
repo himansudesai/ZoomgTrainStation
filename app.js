bdae25dd9eab120adabf781bc541c5607b23c754
Himansu Desai <himansudesai@gmail.com>
2020-12-28 13:11:24 -0800

    We now only need to pass in an empty div to Zoomg and don't need the hacky pre-created HTML that contains <svg><g></g></svg>.  Zoomg takes care of that but it now has a callback argument and we need to continue adding shapes to Zoomg after the html is rendered (in the callback)

--- app.js
+++ app.js
@@ -7,36 +7,37 @@ const ViewStack = Zoomg.ViewStack;
 const Rectangle = Zoomg.Rectangle;
 
 const zoomgContainer = document.getElementById("zoomg-container");
-svgEle.initialize("zoomg-container");
+svgEle.initialize("zoomg-container", () => {
+  const context = new Rectangle("top-context", 0, 0, null, 0, 0);
 
-const context = new Rectangle("top-context", 0, 0, null, 0, 0);
+  const ROW_COUNT = 25;
+  const COLUMN_COUNT = 120;
 
-const ROW_COUNT = 25;
-const COLUMN_COUNT = 120;
+  const ATOM_SIZE = 8;
+  const ENGINE_SIZE = 8;
+  const anAtom = new Atom("dummy", 0, 0, null);
+  const anEngine = new Engine("dummy", 0, 0, null);
+  const ATOM_SCALE = Config.registerInitialSizeForShape(anAtom.getTypeName(), ATOM_SIZE);
+  const ENGINE_SCALE = Config.registerInitialSizeForShape(anEngine.getTypeName(), ENGINE_SIZE);
 
-const ATOM_SIZE = 8;
-const ENGINE_SIZE = 8;
-const anAtom = new Atom("dummy", 0, 0, null);
-const anEngine = new Engine("dummy", 0, 0, null);
-const ATOM_SCALE = Config.registerInitialSizeForShape(anAtom.getTypeName(), ATOM_SIZE);
-const ENGINE_SCALE = Config.registerInitialSizeForShape(anEngine.getTypeName(), ENGINE_SIZE);
-
-let count = 0;
-for (let i = 0; i < COLUMN_COUNT; i++) {
-  for (let j = 0; j < ROW_COUNT; j++) {
-    let atom = new Atom(`${++count}`, (ATOM_SIZE * i), (ATOM_SIZE * j), svgEle.group, ATOM_SCALE);
-    context.insert(atom);
+  let count = 0;
+  for (let i = 0; i < COLUMN_COUNT; i++) {
+    for (let j = 0; j < ROW_COUNT; j++) {
+      let atom = new Atom(`${++count}`, (ATOM_SIZE * i), (ATOM_SIZE * j), svgEle.group, ATOM_SCALE);
+      context.insert(atom);
+    }
   }
-}
 
-for (let i = 0; i < COLUMN_COUNT; i++) {
-  for (let j = 25; j < ROW_COUNT * 2; j++) {
-    let engine = new Engine(`${++count}`, (ENGINE_SIZE * i), (ENGINE_SIZE * j) + 20, svgEle.group, ENGINE_SCALE);
-    context.insert(engine);
+  for (let i = 0; i < COLUMN_COUNT; i++) {
+    for (let j = 25; j < ROW_COUNT * 2; j++) {
+      let engine = new Engine(`${++count}`, (ENGINE_SIZE * i), (ENGINE_SIZE * j) + 20, svgEle.group, ENGINE_SCALE);
+      context.insert(engine);
+    }
   }
-}
 
-Config.sayHello();
-ViewStack.reset(zoomgContainer.clientWidth, zoomgContainer.clientHeight, context);
+  Config.sayHello();
+  console.log(`**** VIEWSTACK RESET`);
+  ViewStack.reset(zoomgContainer.clientWidth, zoomgContainer.clientHeight, context);
 
-window.context = context;
+  window.context = context;
+})
